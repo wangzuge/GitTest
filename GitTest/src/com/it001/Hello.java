f23ac487db92d666a3cb8fe9a33e6313fc30ff52
wangzuge <2366436239@qq.com>
2020-11-07 18:42:24 +0800

    update

--- GitTest/src/com/it001/Hello.java
+++ GitTest/src/com/it001/Hello.java
@@ -1,3 +1,5 @@
+package com.it001;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello!");
---
+++ GitTest/src/com/it001/Test.java
@@ -0,0 +1,7 @@
+package com.it001;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈啊哈哈");
+    }
+}
