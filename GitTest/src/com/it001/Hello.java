1e692362e1b5b9210a3a488fd53cdb403f121509
wangzuge <2366436239@qq.com>
2020-11-08 20:52:57 +0800

    update” â

--- GitTest/src/com/it001/Hello.java
+++ GitTest/src/com/it001/Hello.java
@@ -2,7 +2,6 @@ package com.it001;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("本地代码+github代码！");
-        System.out.println("本地aaagit代码");
+        System.out.println("本地代码");
     }
 }
